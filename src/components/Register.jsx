64762c2344a2c93edd182d7e57bba77e9428a9cd
JimDiazC <jamesherney30@gmail.com>
2023-02-21 13:45:44 -0500

    Registro
    Hacer el registro

---
+++ src/components/Register.jsx
@@ -0,0 +1,48 @@
+import "../styled-sheets/Register.css";
+import { RiArrowRightSLine } from "react-icons/ri";
+import { RiWaterFlashFill } from "react-icons/ri";
+import CarouselDemo from "./CarouselDemo.jsx";
+
+const Register = () => {
+  return (
+    <div className="main">
+      <div className="container_image">
+        <CarouselDemo />
+      </div>
+      <form>
+        <h2 className="container_logo"><RiWaterFlashFill className="title-image" />ServiceGuide</h2>
+        <div className="container-write">
+          <h2 className="title">Sign Up</h2>
+          <div className="container-register">
+            <div className="input-name-lastname">
+              <div>
+                <label className="lbl-title">Name</label>
+                <input className="form-control" type="text" placeholder="Name" />
+              </div>
+              <div>
+                <label className="lbl-title">Last Name</label>
+                <input className="form-control" type="text" placeholder="Last Name" />
+              </div>
+            </div>
+            <div className="input-email">
+              <label htmlFor="">Email</label>
+              <input className="form-control" type="email" placeholder="email" />
+            </div>
+            <div className="input-password">
+              <label htmlFor="">Password</label>
+              <input className="form-control" type="password" placeholder="Password" />
+            </div>
+          </div>
+          <p>Have already an account?</p>
+          <a href="">Sign in</a>
+          <button className="btn-login">
+            SignUp
+            <RiArrowRightSLine className="icon" />
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Register;
