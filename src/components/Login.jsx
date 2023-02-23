5e8f82d55bcc19f23e14e99569a0bd9cc71aba1f
SamuelAroca <samuel.arocad@unac.edu.co>
2023-02-23 03:26:58 -0500

    Show Password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import Swal from "sweetalert2";
 import "../styled-sheets/Login.css";
 import { RiArrowRightSLine } from "react-icons/ri";
 import { RiWaterFlashFill } from "react-icons/ri";
+import { RiEyeLine } from "react-icons/ri";
 import CarouselDemo from "./CarouselDemo.jsx";
+
+window.onload = f => {
+  var eye = document.getElementById('Eye');
+  var input = document.getElementById('Input');
+
+  f.preventDefault();
+  eye.onclick = e => {
+    e.preventDefault();
+    if (input.value.length > 0) {
+      if (input.type == 'password') {
+        input.type = 'text'
+        eye.style.opacity = 0.8
+      } else {
+        input.type = 'password'
+        eye.style.opacity = 0.4
+      }
+    } else {
+      console.log('Paila');
+    }
+  }
+}
 
 const Login = () => {
   return (
@@ -22,11 +45,15 @@ const Login = () => {
               placeholder="Type your email"
             />
             <label className="label-title">Password</label>
-            <input
-              className="input-password form-control"
-              type="password"
-              placeholder="Type your password"
-            />
+            <div className="password-container">
+              <input
+                className="input-password form-control"
+                type="password"
+                placeholder="Type your password"
+                id="Input"
+              />
+              <RiEyeLine className="icon-password" id="Eye" />
+            </div>
             <a href="forgot-password" className="forgot-password">
               Forgot password?
             </a>
