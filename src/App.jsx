69665cba54c169098e96a50365f21dc44c30334f
SamuelAroca <samuel.arocad@unac.edu.co>
2023-03-03 19:48:09 -0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import './App.css'
 import Login from './components/Login'
 import Register from './components/Register'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './components/Index';
 import NavbarComp from './components/NavbarComp';
 
 const App = () => {
 
   return (
-    <div className='container-app'>
+    <Router>
       <NavbarComp />
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Index />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+      <Routes>
+        <Route path='/' element={<Index />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,22 +1,35 @@
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import '../styled-sheets/NavbarComp.css'
 
 const NavbarComp = () => {
+  const [ fix, setFix ] = useState(false)
+
+  function setFixed() {
+  }
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light bg-transparent fixed-top">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">Service<span className='span-guide'>Guide</span></a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon color"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-link" href="/">Home</a>
-            <a className="nav-link" href="/login">Login</a>
-            <a className="nav-link" href="/register">Register</a>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <header>
+      <nav className='navBar'>
+        <Link to='/' className='brand'>Service<span>Guide</span></Link>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>Nosotros</Link>
+          </li>
+          <li>
+            <Link to='/news'>Noticias</Link>
+          </li>
+          <li>
+            <Link to='/register'>Registrar</Link>
+          </li>
+          <li>
+            <Link to='/login'>Ingresar</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
