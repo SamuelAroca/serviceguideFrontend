17f895d33817bd2ca9f43f48e7e8f871f32c7319
SamuelAroca <samuel.arocad@unac.edu.co>
2023-03-03 23:50:47 -0500

    Cambio Navbar y correcciones

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,36 +1,51 @@
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
-import '../styled-sheets/NavbarComp.css'
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "../styled-sheets/NavbarComp.css";
+import { RiWaterFlashFill } from "react-icons/ri";
 
 const NavbarComp = () => {
-  const [ fix, setFix ] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [fix, setFix] = useState(false);
 
   function setFixed() {
+    if (window.scrollY >= 392) {
+      setFix(true);
+    } else {
+      setFix(false);
+    }
   }
-  return (
-    <header>
-      <nav className='navBar'>
-        <Link to='/' className='brand'>Service<span>Guide</span></Link>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>Nosotros</Link>
-          </li>
-          <li>
-            <Link to='/news'>Noticias</Link>
-          </li>
-          <li>
-            <Link to='/register'>Registrar</Link>
-          </li>
-          <li>
-            <Link to='/login'>Ingresar</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
-}
 
-export default NavbarComp
+  window.addEventListener("scroll", setFixed);
+
+  return (
+    <div className="NavbarComp">
+      <Link className="nav_logo_texto" to='/'><RiWaterFlashFill className="nav_logo" />ServiceGuide</Link>
+      <div className={`nav_items ${isOpen && "open"}`}>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/about" className="link">
+          Nosotros
+        </Link>
+        <Link to="/news" className="link">
+          Noticias
+        </Link>
+        <Link to="/register" className="link">
+          Registrar
+        </Link>
+        <Link to="/login" className="link">
+          Ingresar
+        </Link>
+      </div>
+      <div
+        className={`nav_toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarComp;
