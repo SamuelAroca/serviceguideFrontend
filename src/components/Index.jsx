abfb382cd2d0b10fda07d62ea50bbd6b9a776fec
JimDiazC <jamesherney30@gmail.com>
2023-03-07 13:40:00 -0500

    Home
    -Arreglar imágenes
    -Parte del about us

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -6,8 +6,24 @@ import Register from "./Register";
 const Index = () => {
   return (
     <div className={styles.main_container}>
-      <section id="home">
-        <header className={styles.header}></header>
+      <section className={styles.header_section} id="home">
+        <header className={styles.header}>
+          <div className={styles.container_title}>
+            <h1>Management of consumption of <br /> public services.</h1>
+            <h2 className={styles.subtitle_header}>Responsible production and consumption</h2>
+          </div>
+        </header>
+        <div className={styles.statistics_cont}>
+            <div className={styles.sta_1}>
+
+            </div>
+            <div className={styles.sta_2}>
+
+            </div>
+            <div className={styles.sta_3}>
+
+            </div>
+          </div>
       </section>
       <section id="aboutUs">
         <AboutUs />
