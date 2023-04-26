d1506757bf03ce58bad5381f0749d33a27f07386
JimDiazC <101286666+JimDiazC@users.noreply.github.com>
2023-04-26 12:20:29 -0500

    Not Found

--- src/pages/home/components/NotFound.jsx
+++ src/pages/home/components/NotFound.jsx
@@ -5,14 +5,31 @@ import Button from "@mui/material/Button";
 
 const NotFound = () => {
   return (
-    <div className={styled.div_main}>
-      <h3 className={styled.tittle}>404 Not Found</h3>
-      <div className={styled.div_button}>
-        <Button variant="contained" className={styled.button} href="/">
-          Volver
-        </Button>
+    <section className={styled.page_404}>
+      <div className={styled.container}>
+        <div className={styled.row}>
+        <div className={styled.col_sm_12}>
+        <div className={styled.col_sm_10}>
+        <div className={styled.four_zero_four_bg}>
+          <h1 className={styled.text_center}>404</h1>
+          <div className={styled.image}>
+            
+          </div>
+        </div>
+        <div className={styled.h3}>
+          <h3 className={styled.h2}>
+            Look Like you're lost
+          </h3>
+          <p>
+            the page you are looking for not available!
+          </p>
+          <a href="" className={styled.link_404}>Go to Home</a>
+        </div>
+          </div>
+          </div>
+          </div>
       </div>
-    </div>
+    </section>
   );
 };
 
