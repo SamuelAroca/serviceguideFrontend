03079378225c73511b3726c641834cb389ffe016
SamuelAroca <samuel.arocad@unac.edu.co>
2023-05-04 01:47:28 -0500

    Form send email forgot password

--- src/pages/home/components/ForgotPassword.jsx
+++ src/pages/home/components/ForgotPassword.jsx
@@ -9,35 +9,74 @@ import img2 from "../../../assets/alcantarillado.jpg";
 import img3 from "../../../assets/Electricistas-scaled.jpg";
 import img4 from "../../../assets/gas-natural.jpeg";
 import axios from "axios";
+import { Alert } from "@mui/material";
 
 const ForgotPassword = () => {
-  const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState([]);
   const navigate = useNavigate();
+  const [email, setEmail] = useState({
+    mailTo: "",
+  });
 
-  const url = "http://localhost:8080/api/users/auth/login";
+  const url = "http://localhost:8080/api/email/send-email";
 
-  const login = async (e) => {
+  const onValidate = () => {
+    let errors = {};
+    const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // Expresión regular para validar precios
+
+    if (!email.mailTo.trim()) {
+      errors.mailTo = "Debes poner un email";
+    } else if (!regexEmail.test(email.mailTo)) {
+      errors.mailTo = "You must have a valid email format";
+    }
+    return errors;
+  };
+
+  const sendEmail = async (e) => {
     e.preventDefault();
-    try {
-      let response = await axios.post(url, {
-        email: email,
-      });
+    const err = onValidate(email);
+    setErrors(err);
+    if (Object.keys(err).length === 0) {
+      try {
+        let response = await axios.post(url, email);
 
-      if (response.status != 200) {
-        throw new alert("Login Error");
+        if (response.status != 200) {
+          throw new alert("Login Error");
+        } else {
+          Swal.fire(
+            "E-mail sent!",
+            "Go to your email and check the email we sent you!, Check spam",
+            "success"
+          );
+          /* navigate("/"); */
+        }
+      } catch (error) {
+        let response = error;
+        console.log(response.response.data.message);
+        let message = response.response.data.message;
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: message,
+          footer: '<a href="/forgot-password">Forgot password?</a>',
+        });
       }
-    } catch (error) {
-      let response = error;
-      console.log(response.response.data.message);
-      let message = response.response.data.message;
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: message,
-        footer: '<a href="/forgot-password">Forgot password?</a>',
+
+      setEmail({
+        mailTo: "",
       });
+    } else {
+      setErrors(err);
     }
   };
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setEmail((prevEmail) => ({
+      ...prevEmail,
+      [name]: value,
+    }));
+  }
 
   return (
     <div className={styles.components}>
@@ -51,7 +90,7 @@ const ForgotPassword = () => {
       </div>
       <div className={styles.container}>
         <div className={styles.container_login}>
-          <form className={styles.container_form}>
+          <form className={styles.container_form} onSubmit={sendEmail}>
             <h2 className={styles.container_logo}>
               <RiWaterFlashFill className={styles.logo} />
               ServiceGuide
@@ -66,13 +105,17 @@ const ForgotPassword = () => {
                     placeholder="Type your email"
                     className={styles.inputsMaterial}
                     fullWidth
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    name="mailTo"
+                    value={email.mailTo}
+                    onChange={handleInputChange}
                   />
+                  {errors.mailTo && (
+                    <Alert severity="warning"> {errors.mailTo} </Alert>
+                  )}
                 </div>
               </div>
               <div className={styles.div_button_login}>
-                <button className={styles.login_button} onClick={login}>
+                <button className={styles.login_button} onClick={sendEmail}>
                   Send Email
                   <div class={styles.arrow_wrapper}>
                     <div class={styles.arrow}></div>
